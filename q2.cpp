75dcd74e43e58830f8301a04e2c7b810cd7d8c9e
PranshuSehra30 <110589138+PranshuSehra30@users.noreply.github.com>
2022-08-26 15:28:32 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -4,13 +4,14 @@ int main()
 {
  // try catch block  to handle errors
     try {
-  
+//   number of elemnts in array
      int n;
     cin >> n;
     int arr[n];
+//      taking array intput
     for (auto &it : arr)
         cin >> it;
-
+//  creatinng a map of int to vector<int> to store the locations of previous sum
     map<int, vector<int>> m;
     int sum = 0;
     m[0] = {-1};
